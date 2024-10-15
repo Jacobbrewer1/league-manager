b44786fe0b54e8e0e2fe20c81d501061c2645261
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-15 20:21:04 +0100

    Init

--- pkg/services/api/players.go
+++ pkg/services/api/players.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Jacobbrewer1/league-manager/pkg/codegen/apis/api"
 )
 
-func (s service) GetPlayers(w http.ResponseWriter, r *http.Request, params api.GetPlayersParams) {
-	// TODO: Implement
-	panic("not implemented")
+func (s *service) GetPlayers(w http.ResponseWriter, r *http.Request, params api.GetPlayersParams) {
+	//TODO implement me
+	panic("implement me")
 }
