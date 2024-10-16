c2b84169b7bd72033c9c9b1c178f268d70d30bed
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-16 11:38:34 +0100

    fix(players): Updating name like filters (#8)
    Updating name like filters

---
+++ pkg/repositories/api/filters/firstname_like.go
@@ -0,0 +1,23 @@
+package filters
+
+import "github.com/Jacobbrewer1/pagefilter"
+
+type firstnameLike struct {
+	fnl string
+}
+
+// NewFirstnameLike creates a new instance of a firstname like filter.
+func NewFirstnameLike(fn string) pagefilter.Wherer {
+	return &firstnameLike{
+		fnl: fn,
+	}
+}
+
+// Where returns the where clause for the filter.
+func (f *firstnameLike) Where() (string, []interface{}) {
+	return "first_name LIKE ?", []interface{}{f.fnl}
+}
+
+func (f *firstnameLike) WhereType() pagefilter.WhereType {
+	return pagefilter.WhereTypeOr
+}
