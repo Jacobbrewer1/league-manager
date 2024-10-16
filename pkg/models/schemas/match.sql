aabee28291f81898e8064df44e25cce8553e9793
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-16 15:31:55 +0100

    Updating the models

--- pkg/models/schemas/match.sql
+++ pkg/models/schemas/match.sql
@@ -1,10 +1,13 @@
 create table `match`
 (
     id               int      not null auto_increment,
+    season_id        int      not null,
     home_partners_id int      not null,
     away_partners_id int      not null,
     match_date       datetime not null,
     primary key (id),
+    constraint match_season_id_fk
+        foreign key (season_id) references season (id),
     constraint matches_partnership_id_fk
         foreign key (home_partners_id) references partnership (id),
     constraint matches_partnership_id_fk2
