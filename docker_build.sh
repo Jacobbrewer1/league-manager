4d25b0b4d14a53a2d849ee3c3caece113c85856a
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-18 20:15:58 +0100

    fix(games): Updating pagefilter table (#22)
    * Updating pagefilter table
    
    * Updating the docker cicd

--- docker_build.sh
+++ docker_build.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-buildApp=$1
-toPush=$2
+buildApp=""
+toPush=false
+forced=false
 
 function fail() {
-  echo "Error: $1"
+  gum style --foreground 196 "Error: $1"
   exit 1
 }
+
+function print_usage() {
+  printf "Usage: docker_build.sh [OPTIONS]\n"
+}
+
+while getopts 'abfp' flag; do
+  case "${flag}" in
+  a)
+    buildApp="build-all"
+    ;;
+  b)
+    if [ "$buildApp" != "" ]; then
+      print_usage
+      exit 1
+    fi
+    shift
+    buildApp="$1"
+    ;;
+  f)
+    forced=true
+    ;;
+  p)
+    toPush=true
+    ;;
+  *)
+    print_usage
+    exit 1
+    ;;
+  esac
+done
 
 function handle_subdirectory() {
   hash=$(git rev-parse --short HEAD)
@@ -16,7 +47,7 @@ function handle_subdirectory() {
   echo "$DATE"
 
   # Build binary for the subdirectory
-  echo "Building binary for $1"
+  gum style --foreground 10 "Building binary for $1"
   cd ./"$1" || fail "Unable to cd to cmd/$1"
 
   GOOS=linux GOARCH=amd64 go build -o bin/app -ldflags "-X main.Commit=$hash -X main.Date=$DATE" . || fail "Unable to build binary for $1"
@@ -34,14 +65,19 @@ function handle_subdirectory() {
   appName=${appName#"cmd/"}
 
   # Build the docker image
-  echo "Building docker image for $appName"
+  gum style --foreground 10 "Building docker image for $appName"
   registry="ghcr.io/jacobbrewer1/league-manager-$appName"
 
-  docker build -t "$registry:$hash" -t "$registry:latest" . || fail "Unable to build docker image for $appName"
+  gum spin --spinner dot --title "Building image" -- docker build -t "$registry:$hash" -t "$registry:latest" . || fail "Unable to build docker image for $appName"
+
+  if [ "$forced" == false ]; then
+    gum style --foreground 10 "Would you like to push the docker image to the GitHub Container Registry?"
+    gum confirm && toPush=true
+  fi
 
   # Check if the toPush variable is set to true. If yes, push the docker image to the GitHub Container Registry
-  if [ "$toPush" != "true" ]; then
-    echo "Not pushing docker image to GitHub Container Registry"
+  if [ "$toPush" != true ]; then
+    gum style --foreground 10 "Docker image built for $appName"
 
     # Cleanup the binary
     rm -rf bin
@@ -51,15 +87,15 @@ function handle_subdirectory() {
   fi
 
   # Push the docker image to the GitHub Container Registry
-  echo "Pushing docker image to GitHub Container Registry"
+  gum style --foreground 10 "Pushing docker image to GitHub Container Registry"
 
   docker push "$registry:$hash" || fail "Unable to push docker image to GitHub Container Registry"
-  echo "Docker image pushed to $registry:$hash"
+  gum style --foreground 10 "Docker image pushed to $registry:$hash"
 
   docker push "$registry:latest" || fail "Unable to push docker image to GitHub Container Registry"
-  echo "Docker image pushed to $registry:latest"
+  gum style --foreground 10 "Docker image pushed to $registry:latest"
 
-  echo "Done building $appName"
+  gum style --foreground 10 "Docker image pushed to $registry:$hash"
 
   # Cleanup the binary
   rm -rf bin
@@ -71,24 +107,28 @@ if [ -z "$toPush" ]; then
   toPush="false"
 fi
 
+subdirectories=$(ls -d ./cmd/*)
+
 if [ "$buildApp" == "build-all" ]; then
-  echo "Building all apps"
-elif [ -z "$buildApp" ]; then
-  echo "No app specified. Building all apps"
-  buildApp="build-all"
+  gum style --foreground 196 "Building all apps"
+elif [ "$buildApp" == "" ]; then
+  apps=$(find cmd -mindepth 1 -type d -exec basename {} \; | sed 's/^cmd\///g')
+  buildApp=$(echo "$apps" | gum choose --no-limit --header "Select the app(s) to generate")
+  if [ -z "$buildApp" ]; then
+    gum style --foreground 196 "No app selected"
+    exit 0
+  fi
+  subdirectories="cmd/$buildApp"
 else
-  echo "Building $buildApp"
+  gum style --foreground 196 "Building $buildApp"
   handle_subdirectory "cmd/$buildApp"
   exit 0
 fi
 
-# Get all the subdirectories in the cmd directory
-subdirectories=$(ls -d ./cmd/*)
-
 # For each subdirectory of the cmd directory and run the subdirectory function
 for dir in $subdirectories; do
   if [ -d "$dir" ]; then
-    echo "Running $dir"
+    gum style --foreground 10 "Building $dir"
     handle_subdirectory "$dir"
   fi
 done
